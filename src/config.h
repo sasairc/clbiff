1f7a6fff8080cd1309ec5163806fb81f4414e801
sasairc <sasairc@ssiserver.moe.hm>
2015-09-22 18:15:47 +0900

    clbiff 0.6.1-devel (x86_64-linux-gnu)

--- src/config.h
+++ src/config.h
@@ -16,8 +16,8 @@
 #define PROGNAME        "clbiff"
 #define VERSION         0
 #define PATCHLEVEL      6
-#define SUBLEVEL        0
-#define EXTRAVERSION    ""
+#define SUBLEVEL        1
+#define EXTRAVERSION    "-devel"
 
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
